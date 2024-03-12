7cf1fef4f565b3885d5deec74bb6183157637318
PHSS10 <pedrohenriquesousas14@gmail.com>
2024-03-12 15:57:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 function isPalindrome(str){
-    let ultimaString = str.toLowerCase().length -1;
+    
+    const conversao = str.toLowerCase().replaceAll(" ","");
+    const ultimaString = conversao.length -1;
     let result = ""
     
     for(let i = ultimaString; i >= 0; i--){
-
-        result= str[i] + result ;
-        console.log(result);
+   
+        result= result + conversao[i] ;
+       
     }
-    if(result == str){
-        return true
+    if(result == conversao){
+        return true;
     } 
     
-    return false
+    return false;
 }
-console.log(isPalindrome("Anilina"))
+
+function arrayMaxMin(arr){
+    
+    let valorMax = arr[0];
+    let valorMin = arr[0];
+    let novoArr = [valorMin, valorMax];
+    
+    for(let i = 0; i < arr.length; i ++){
+
+        if(arr[i] >= valorMax){
+            
+            valorMax = arr[i];
+        } 
+        else if(arr[i] <= valorMin){
+            
+            valorMin = arr[i]; 
+        }
+    }
+    
+   novoArr=[valorMin ,valorMax];
+   return novoArr;
+}
+
+    
